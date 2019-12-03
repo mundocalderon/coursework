cc0a39fa8cfaeae1fb81bb4c80c17ef5c6de7a9b
mundocalderon <mundocalderon@gmail.com>
2019-12-03 15:13:22 -0800

    started mondo course and finished first assignment

--- jhu-unix-workbench/Commands/commands.sh
+++ jhu-unix-workbench/Commands/commands.sh
@@ -1,2 +1,3 @@
-source ./server_copy.sh
-source ./short
+
+source /Users/admin/Projects/Coursework/jhu-unix-workbench/Commands/server_copy.sh
+source /Users/admin/Projects/Coursework/jhu-unix-workbench/Commands/short
