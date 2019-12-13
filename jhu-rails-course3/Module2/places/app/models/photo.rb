b875ae49363cc4b2018a68afb1763acadd373f71
mundocalderon <mundocalderon@gmail.com>
2019-12-13 14:07:18 -0800

    finished the MongoDB exercise. AggregationPipeline, Indexing, GridFS, one-to-many relationships, Geolocation

---
+++ jhu-rails-course3/Module2/places/app/models/photo.rb
@@ -0,0 +1,106 @@
+class Photo
+	require 'EXIFR/JPEG'
+
+	attr_accessor :id, :location, :place
+	attr_writer :contents
+
+	def self.mongo_client
+		Mongoid::Clients.default
+	end
+
+	def initialize(params={})
+		@id= params[:_id].nil? ? params[:id] : params[:_id].to_s
+		if !params[:metadata].nil?
+			@location= params[:metadata][:location].nil? ? nil : Point.new(params[:metadata][:location])
+			@place= params[:metadata][:place].nil? ? nil : params[:metadata][:place]
+		end
+	end
+
+	def persisted?
+		!@id.nil?
+	end
+
+	def contents
+	    f=self.class.mongo_client.database.fs.find_one(:_id=>BSON::ObjectId.from_string(@id))
+	    if f 
+	      buffer = ""
+	      f.chunks.reduce([]) do |x,chunk| 
+	          buffer << chunk.data.data 
+	      end
+	      return buffer
+	    end 
+	end
+
+	def place
+		@place.nil? ? nil : Place.find(@place)
+	end
+
+	def place=(new_place)
+		case 
+		when new_place.is_a?(Place)
+			@place= BSON::ObjectId.from_string(new_place.id)
+		when new_place.is_a?(String)
+			@place= BSON::ObjectId.from_string(new_place)
+		when new_place.is_a?(BSON::ObjectId)
+			@place = new_place
+		when new_place.nil?
+			@place = nil
+		end
+	end
+
+	def save  	
+		if !self.persisted?  
+			description = {}  		 		
+			gps = EXIFR::JPEG.new(@contents).gps	
+			@location = Point.new(:lng => gps.longitude, :lat => gps.latitude) 
+
+			description[:content_type] = "image/jpeg"  		
+			description[:metadata] = {}  		
+			description[:metadata][:location] = @location.to_hash
+			description[:metadata][:place] = @place  		
+			if @contents  			
+				@contents.rewind  			
+				grid_file = Mongo::Grid::File.new(@contents.read, description)  			
+				id = self.class.mongo_client.database.fs.insert_one(grid_file)  			
+				@id = id.to_s  			
+				return @id  		
+			end  	
+		else
+			self.class.mongo_client.database.fs.find(:_id=>BSON::ObjectId.from_string(@id)).
+												update_one(:$set=>{:'metadata.place'=>@place, :'metadata.location' =>@location.to_hash})
+		end
+
+	end
+
+	def destroy
+		id_criteria = BSON::ObjectId.from_string(@id)
+	    self.class.mongo_client.database.fs.find(:_id=>id_criteria).delete_one
+	end
+
+	def self.all(offset=0,limit=nil)
+		result=[]
+		photos= self.mongo_client.database.fs.find
+		photos=photos.skip(offset) if offset
+		photos=photos.limit(limit) if limit
+		photos.map{|doc| result << Photo.new(doc)}
+		result
+	end
+
+	def self.find(id)
+    	bson_id = BSON::ObjectId.from_string(id)
+    	doc = self.mongo_client.database.fs.find(:_id=>bson_id).first
+    	photo = doc.nil? ? nil : Photo.new(doc)
+
+	end
+
+	def self.find_photos_for_place(place_id)
+		place_id = BSON::ObjectId.from_string(place_id.to_s) #This will make sure that we are working with a BSON ObjectID
+		photos = self.mongo_client.database.fs.find(:'metadata.place'=>place_id)  
+	end
+
+	def find_nearest_place_id(max_meters)
+		place = Place.near(@location, max_meters).limit(1).projection(:_id=>1).first
+		place.nil? ? 0 : place[:_id]
+	end
+
+end
--- jhu-rails-course3/Module2/places/app/models/place.rb
+++ jhu-rails-course3/Module2/places/app/models/place.rb
@@ -1,5 +1,6 @@
 
 class Place
+  include ActiveModel::Model
   attr_accessor :id, :formatted_address, :location, :address_components
 
   def initialize(hash)
@@ -13,7 +14,21 @@ class Place
     end
    
     @formatted_address = hash[:formatted_address]
-    @location = Point.new(hash[:geometry][:location]) 
+
+    if hash[:geometry][:location]
+      @location = Point.new(hash[:geometry][:location]) 
+    elsif hash[:geometry][:geolocation]
+      @location = Point.new(hash[:geometry][:geolocation])
+    end
+      
+  end
+
+  def persisted?
+    !@id.nil?
+  end
+
+  def destroy
+    self.class.collection.find({_id: BSON::ObjectId.from_string(@id)}).delete_one
   end
 
   def self.mongo_client
@@ -28,7 +43,88 @@ class Place
   def self.load_all(file_path) 
     file=File.read(file_path)
     self.collection.insert_many(JSON.parse(file))
+  end
 
+  def self.find_by_short_name(name)
+    self.collection.find('address_components.short_name'=> name)
+  end
+
+  def self.find id
+    bson_id = BSON::ObjectId.from_string(id)
+    doc = self.collection.find(_id: bson_id).first
+    doc.nil? ? nil : Place.new(doc)
+  end
+
+  def self.all(offset=0, limit=nil)
+    coll=[]
+    result=self.collection.find.skip(offset)
+    result=result.limit(limit) if !limit.nil?
+    if !result.nil?
+      result.each do |doc|
+        coll << Place.new(doc)
+      end
+    end
+    coll
+  end
+
+  def self.to_places(input)
+    coll=[]
+    if !input.nil?
+      input.each do |doc|
+        coll << Place.new(doc)
+      end
+    end
+    coll
+  end
+
+  def self.get_address_components(sort=nil,offset=0,limit=nil)
+    aggregation = self.collection.aggregate([ {:$unwind=>"$address_components"}, {:$project=>{ address_components:1, formatted_address:1, 'geometry.geolocation'=>1}} ])
+    aggregation.pipeline << {:$sort=> sort} if !sort.nil?
+    aggregation.pipeline << {:$skip=>offset} if offset
+    aggregation.pipeline << {:$limit=>limit} if !limit.nil?
+    aggregation
+  end
+
+  def self.get_country_names
+    countries = self.collection.aggregate([ {:$unwind => "$address_components"},
+                                                {:$match=>{'address_components.types'=>'country'}},
+                                                {:$project=>{_id:0, 'address_components.long_name'=>1, 'address_components.types'=>1}},
+                                                {:$group=>{_id:'$address_components.long_name'}}
+                                              ]).to_a.map{|h| h[:_id]}
+  end
+
+  def self.find_ids_by_country_code(country_code)
+    ids= self.collection.aggregate([ {:$unwind => "$address_components"},
+                                        {:$match=>{'address_components.types'=>'country', 'address_components.short_name'=>country_code}},
+                                        {:$project=>{_id:1}}
+                                    ]).map {|doc| doc[:_id].to_s}
+  end
+
+  def self.create_indexes
+    Place.collection.indexes.create_one({'geometry.geolocation'=>Mongo::Index::GEO2DSPHERE})
+  end
+
+  def self.remove_indexes
+    Place.collection.indexes.drop_one('geometry.geolocation_2dsphere')
+  end
+
+  def self.near(coordinate, max_meters=nil)
+    near_query= {:$geometry=>coordinate.to_hash}
+    near_query[:$maxDistance]=max_meters if max_meters
+    collection.find(:"geometry.geolocation"=>{:$near=>near_query})
+    #AKA self.collection.find(:"geometry.geolocation"=>{ :$near=>{:$geometry=>coordinate.to_hash}, :$maxDistance=>max_meters })
+  end
+
+  def near(max_meters=nil)
+    self.class.to_places(self.class.near(@location,max_meters))
+  end
+
+  def photos(offset=0, limit=nil)
+    result=[]
+    photos = Photo.find_photos_for_place(self.id).skip(offset)
+    photos = photos.limit(limit) if !limit.nil?
+    photos.map{ |p| result << Photo.new(p)}
+    result
   end
 
 end
